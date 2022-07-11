a75a7547eead8013c22a1ff8afd4bb529652108a
Hannah DeFazio <hannah.defazio@kitware.com>
2022-07-11 12:41:25 -0400

    Formatting

--- unity/ARUI/Assets/Scripts/TapQueryTaskGraph.cs
+++ unity/ARUI/Assets/Scripts/TapQueryTaskGraph.cs
@@ -7,37 +7,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class TapQueryTaskGraph : MonoBehaviour, IMixedRealityInputActionHandler
 {
-    /*string[,] tasks =
-    {
-        {"0", "Boil 12 ounces of water"},
-        {"1", "Measure 12 ounces of water in the liquid measuring cup"},
-        {"1", "Pour the water from the liquid measuring cup into  the electric kettle"},
-        {"1", "Turn on the electric kettle by pushing the button underneath the handle"},
-        {"1", "Boil the water. The water is done boiling when the button underneath the handle pops up"},
-
-        {"0","Assemble the filter cone"},
-        {"1", "While the water is boiling, assemble the filter cone. Place the dripper on top of a coffee mug"},
-        {"1", "Prepare the filter insert by folding the paper filter in half to create a semi-circle, and in half again to create a quarter-circle. Place the paper filter in the dripper and spread open to create a cone."},
-        {"1", "Take the coffee filter and fold it in half to create a semi-circle"},
-        {"1", "Folder the filter in half again to create a quarter-circle"},
-        {"1", "Place the folded filter into the dripper such that the the point of the quarter-circle rests in the center of the dripper"},
-        {"1", "Spread the filter open to create a cone inside the dripper"},
-        {"1", "Place the dripper on top of the mug"},
-        
-        {"0","Ground the coffee and add to the filter cone"},
-        {"1","Weigh the coffee beans and grind until the coffee grounds are the consistency of coarse sand, about 20 seconds. Transfer the grounds to the filter cone."},
-        {"1","Turn on the kitchen scale"},
-
-         {"0","Check the water temperature"},
-         {"1"," Turn on the thermometer"},
-         {"1"," Place the end of the thermometer into the water. The temperature should read 195-205 degrees Fahrenheit or between 91-96 degrees Celsius."},
-
-         {"0","Pour the water over the coffee grounds"},
-         
-         {"0","Clean up the paper filter and coffee grounds"},
-    };*/
     private Logger _logger = null;
 
     ROSConnection ros;
@@ -67,7 +39,7 @@ public class TapQueryTaskGraph : MonoBehaviour, IMixedRealityInputActionHandler
     {
         // Create the QueryTaskGraph subscriber
         ros = ROSConnection.GetOrCreateInstance();
-        ROSConnection.GetOrCreateInstance().Subscribe<TaskGraphMsg>(querytaskgraphTopicName, QueryTaskGraphCallback);
+        ros.Subscribe<TaskGraphMsg>(querytaskgraphTopicName, QueryTaskGraphCallback);
 
         timerTest = new GameObject("TimerTest").AddComponent<Timer>();
     }
@@ -108,24 +80,26 @@ public class TapQueryTaskGraph : MonoBehaviour, IMixedRealityInputActionHandler
     void QueryTaskGraphCallback(TaskGraphMsg msg)
     {
         Logger log = logger();
-        log.LogInfo("Got taskGraphMsg: " + msg)
-        edges = msg.node_edges
+        log.LogInfo("Got taskGraphMsg: " + msg);
+        int[,] edges = msg.node_edges;
         string[,] tasks = {};
         
-        taskIdx = 0;
-        curTask = edges[taskIdx];
+        int taskIdx = 0;
+        int curTask = edges[taskIdx];
         tasks.append({"0", msg.task_nodes[curTask]});
 
-        nextTask = edges[taskIdx+1];
+        int nextTask = edges[taskIdx+1];
 
         // remove pair form list
         edges.erase(edges.begin() + (taskIdx+1));
         edges.erase(edges.begin() + taskIdx);
 
-        for (int i=0; i<=len(edges)/2; i++) // number of pairs to loop through
+        int num_pairs = (sizeof(edges)/sizeof(edges[0]))/2;
+
+        for (int i=0; i<=num_pairs; i++)
         {
             curTask = nextTask;
-            taskIdx = std::distance(edges, std::find(edges.begin(), edges.end(), curTask))
+            taskIdx = std::distance(edges, std::find(edges.begin(), edges.end(), curTask));
             tasks.append({"0", msg.task_nodes[curTask]});
 
             nextTask = edges[taskIdx+1];
@@ -138,7 +112,7 @@ public class TapQueryTaskGraph : MonoBehaviour, IMixedRealityInputActionHandler
         tasks.append({"0", msg.task_nodes[nextTask]});
 
         // Send tasks to ARUI
-        log.LogInfo("Setting task list: " + tasks)
+        log.LogInfo("Setting task list: " + tasks);
         AngelARUI.Instance.SetTasks(tasks);
     }
 
